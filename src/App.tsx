bd5560c527a150c6dbd5f7cf3fe86fc27f3a65c8
Wevison Ramalho Silva <wevison.ramalho@afferolab.com.br>
2020-11-02 19:16:28 -0300

    [ FEATURE ] Adding style in the landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logo from './images/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <p className="text">
-        Hello World!
-      </p>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logo} alt="Happy" />
+
+        <main className="main">
+          <h1 className="title">Leve felicidade para o mundo</h1>
+          <p className="text">Visite organatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong className="location-title">Rio do Sul</strong>
+          <span className="location-text">Santa Catarina</span>
+        </div>
+
+        <a href="#" className="enter-app">
+          <FiArrowRight size="26" color="rgba(0, 0, 0, .6)" />
+        </a>
+      </div>
     </div>
   );
 }
